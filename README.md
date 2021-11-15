8f813f9977ae92fa1267e4a0fda64e27083442b5
beamchristian <beamchristian@yahoo.com>
2021-11-14 20:18:28 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# node-employee-tracker
+# Node Employee Tracker
+
+[![Generic badge](https://img.shields.io/badge/License-MIT-yellowgreen.svg)](https://shields.io/)
+
+## Description
+
+A command line application to manage a company's employee database, using Node.js, Inquirer, and MySql.
+
+## License
+
+This application is covered under the MIT License.
+
+## Table of Contents
+
+- [installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Credits](#credits)
+
+## Video
+
+[Video of usage](https://watch.screencastify.com/v/hPBiQIGS2lIJ13HLCTKr)
+
+## Installation
+
+run the following in the command line:
+
+```md
+npm install mysql2
+```
+
+```md
+npm install inquirer
+```
+
+```md
+npm install console.table --save
+```
+
+in terminal
+
+## Usage
+
+type node index in the command line at the applications root directory
+
+## Credits
+
+Node.js, Inquirer, and MySql
+
+## Contributing
+
+For contributing to this project, please follow the guidelines of the:
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)
+
+## Questions
+
+- [GitHub](https://github.com/beamchristian 'GitHub')
+- [Email](mailto:beamchristian@yahoo.com 'Email')
+- [Repo](https://github.com/beamchristian/node-employee-tracker 'Repo')
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
+// inquirer init
+promptQuestions();
 
-// display employee table
-
-const promptQuestions = () => {
+function promptQuestions() {
   return inquirer
     .prompt({
       type: 'list',
       choices: [
         'View all Employees',
         'View all Employees By Department',
+        'View all Roles',
         'Add Employee',
         'Add Role',
         'Add Department',
@@ -21,59 +23,147 @@ const promptQuestions = () => {
       message: 'What would you like to do?',
       name: 'option',
     })
-    .then(answer => {});
-};
+    .then(answer => {
+      switch (answer.option) {
+        case 'View all Employees':
+          return viewEmployees();
+        case 'View all Employees By Department':
+          return viewDepartments();
+        case 'View all Roles':
+          return viewRoles();
+        case 'Add Employee':
+          return addEmployee();
+        case 'Add Role':
+          return addRole();
+        case 'Add Department':
+          return addDepartment();
+        case 'Update Employee role':
+          return updateRole();
+        case 'Remove Employee':
+          return removeEmployee();
+        case 'Quit':
+          return quit();
+      }
+    });
+}
 
 // view all employees
 function viewEmployees() {
   db.query(`SELECT * FROM employee`, (err, result) => {
     if (err) throw err;
+    console.log(`
+    -------------------------------------------`);
     console.table(result);
   });
+  promptQuestions();
 }
 
 function viewDepartments() {
   db.query(`SELECT * FROM department`, (err, result) => {
     if (err) throw err;
+    console.log(`
+    -------------------------------------------`);
     console.table(result);
   });
+  promptQuestions();
 }
 
-function viewRole() {
-  db.query(`SELECT * FROM roles`, (err, result) => {
+function viewRoles() {
+  db.query(
+    `SELECT id, title, salary, department_id FROM roles`,
+    (err, result) => {
+      if (err) throw err;
+      console.log(`
+    -------------------------------------------`);
+      console.table(result);
+    }
+  );
+  promptQuestions();
+}
+
+function addEmployee() {
+  let rolesArray = [
+    'Sales Lead',
+    'Salesperson',
+    'Lead Engineer',
+    'Software Engineer',
+    'Account Manager',
+    'Accountant',
+    'Legal Team Lead',
+    'Lawyer',
+  ];
+
+  return inquirer
+    .prompt([
+      {
+        message: 'Choose an employee role',
+        type: 'list',
+        name: 'role',
+        choices: rolesArray,
+      },
+      {
+        message: 'Set the employees first name',
+        type: 'input',
+        name: 'firstName',
+      },
+      {
+        message: 'Set the employees last name',
+        type: 'input',
+        name: 'lastName',
+      },
+    ])
+    .then(answers => {
+      db.query(
+        `INSERT INTO employee SET ?`,
+        {
+          first_name: answers.firstName,
+          last_name: answers.lastName,
+          role_id: rolesArray.indexOf(answers.role) + 1,
+        },
+
+        (err, result) => {
+          if (err) throw err;
+        }
+      );
+      console.log(answers.role);
+      promptQuestions();
+    });
+}
+
+function addRole(role) {
+  db.query(`INSERT INTO roles SET ?`, role, (err, result) => {
     if (err) throw err;
+    console.log(`
+    -------------------------------------------`);
     console.table(result);
   });
+  promptQuestions();
 }
 
-// function calls
-// viewDepartments();
-// viewRole();
+function addDepartment(department) {
+  db.query(`INSERT INTO department SET ?`, department, (err, result) => {
+    if (err) throw err;
+    console.log(`
+    -------------------------------------------`);
+    console.table(result);
+  });
+  promptQuestions();
+}
 
-// Get a single employee
-// db.query(`SELECT * FROM Employee WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+function updateRole(employeeId, roleId) {
+  db.query(
+    `UPDATE employee SET role_id = ? WHERE id =?`,
+    [roleId, employeeId],
+    (err, result) => {
+      if (err) throw err;
+      console.log(`
+    -------------------------------------------`);
+      console.table(result);
+    }
+  );
+  promptQuestions();
+}
 
-// Delete a Employee
-// db.query(`DELETE FROM Employee WHERE id = ?`, 10, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-// Create a Employee
-// const sql = `INSERT INTO Employee (id, first_name, last_name, role_id, manager_id)
-//               VALUES (?,?,?,?,?)`;
-// const params = [10, 'Jim', 'Beam', 1, 2];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+function quit() {
+  process.exit();
+}
